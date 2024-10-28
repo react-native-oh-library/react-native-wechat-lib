91be4482de8d87662f9003797ad8ba626bc9c993
zhengjinshou <>
2024-10-28 17:16:32 +0800

    feat: App修改为用例模式

--- example/App.tsx
+++ example/App.tsx
@@ -1,44 +1,31 @@
 import React, { useState } from 'react';
 import {
-  Button,
-  Alert,
   View,
   StyleSheet,
-  ScrollView,
   Text,
+  ScrollView,
+  Button,
+  Alert,
   Image,
 } from 'react-native';
+import { Tester, TestCase, TestSuite } from '@rnoh/testerino';
 import * as WeChat from '@react-native-ohos/react-native-wechat-lib';
 import { APP_ID, APP_SECRET } from './src/constants';
 import { launchImageLibrary } from 'react-native-image-picker';
 
 const App = () => {
+  const [authRes, setAuthRes] = useState('');
   const [qrcode, setQrcode] = useState('');
+  const [scanRes, setScanRes] = useState('');
 
   const handleRegisterApp = async () => {
     const res = await WeChat.registerApp(APP_ID, '');
-    console.log(
-      '%c  WechatLibTurboModuleLogger registerApp res:',
-      'color: #0e93e0;background: #aaefe5;',
-      res,
-    );
     Alert.alert(JSON.stringify(res));
-  };
-
-  const handleIsWXAppInstalled = async () => {
-    WeChat.isWXAppInstalled().catch(err => {
-      Alert.alert(JSON.stringify(err.message));
-    });
   };
 
   const handleOpenWXApp = async () => {
     try {
-      const res = await WeChat.openWXApp();
-      console.log(
-        '%c WechatLibTurboModuleLogger openWXApp res:',
-        'color: #0e93e0;background: #aaefe5;',
-        res,
-      );
+      await WeChat.openWXApp();
     } catch (error) {
       console.log('%c  error:', 'color: #0e93e0;background: #aaefe5;', error);
     }
@@ -47,11 +34,7 @@ const App = () => {
   const handleSendAuthRequest = async () => {
     try {
       const res = await WeChat.sendAuthRequest('snsapi_userinfo', 'none');
-      console.log(
-        '%c WechatLibTurboModuleLogger sendAuthRequest res:',
-        'color: #0e93e0;background: #aaefe5;',
-        res,
-      );
+      setAuthRes(res as any);
     } catch (error) {
       console.log('%c  error:', 'color: #0e93e0;background: #aaefe5;', error);
     }
@@ -59,40 +42,11 @@ const App = () => {
 
   const handleAuthByScanRequest = async () => {
     try {
-      const ret = await WeChat.authByScan(APP_ID, APP_SECRET, qrcode => {
-        console.log(
-          '%c WechatLibTurboModuleLogger authByScan qrcode:',
-          'color: #0e93e0;background: #aaefe5;',
-          qrcode,
-        );
+      const res = await WeChat.authByScan(APP_ID, APP_SECRET, qrcode => {
         // 拿到 qrcode 用 Image 去渲染
         setQrcode(qrcode);
       });
-      console.log(
-        '%c WechatLibTurboModuleLogger ret:',
-        'color: #0e93e0;background: #aaefe5;',
-        ret,
-      );
-    } catch (error) {
-      console.log('%c  error:', 'color: #0e93e0;background: #aaefe5;', error);
-    }
-  };
-
-  const handlePay = async () => {
-    try {
-      const res = await WeChat.pay({
-        partnerId: '2480306091',
-        prepayId: 'wx26161523845794ecced251acf2b6860000',
-        nonceStr: 'vmall_240926161523_993_2774',
-        timeStamp: '1727338524',
-        package: 'Sign=WXPay',
-        sign: 'rAqsrx5yLfRNBGvlHYuLhUsNK0OPeOLQ5xlvhxFo9guPU4HeNtzRdPaGAXAzXvn7V5chVe8sj3BfvDgwXlCKctCcFIllOgheyZbZ7btFC',
-      });
-      console.log(
-        '%c WechatLibTurboModuleLogger pay res:',
-        'color: #0e93e0;background: #aaefe5;',
-        res,
-      );
+      setScanRes(res as any);
     } catch (error) {
       console.log('%c  error:', 'color: #0e93e0;background: #aaefe5;', error);
     }
@@ -100,11 +54,10 @@ const App = () => {
 
   const handleShareText = async () => {
     try {
-      const res = await WeChat.shareText({
+      await WeChat.shareText({
         text: '测试一下分享功能',
         scene: 0,
       });
-      console.log('%c WechatLibTurboModuleLogger res:', 'color: #0e93e0;background: #aaefe5;', res);
     } catch (error) {
       console.log('%c  error:', 'color: #0e93e0;background: #aaefe5;', error);
     }
@@ -113,81 +66,116 @@ const App = () => {
   const handleShareRemoteImage = async () => {
     try {
       const res = await WeChat.shareImage({
-        imageUrl: 'https://img.tukuppt.com/photo-big/00/10/77/619619681755c5463.jpg',
+        imageUrl:
+          'https://img.tukuppt.com/photo-big/00/10/77/619619681755c5463.jpg',
         scene: 0,
       });
-      console.log('%c WechatLibTurboModuleLogger handleShareRemoteImage res:', 'color: #0e93e0;background: #aaefe5;', res);
+      console.log(
+        '%c WechatLibTurboModuleLogger handleShareRemoteImage res:',
+        'color: #0e93e0;background: #aaefe5;',
+        res,
+      );
     } catch (error) {
       console.log('%c  error:', 'color: #0e93e0;background: #aaefe5;', error);
     }
   };
 
   const handleShareLocalImage = async () => {
-    try {
-      // const res = await WeChat.shareImage({
-      //   text: '测试一下分享功能',
-      //   scene: 0,
-      // });
-      // console.log('%c WechatLibTurboModuleLogger res:', 'color: #0e93e0;background: #aaefe5;', res);
-    } catch (error) {
-      console.log('%c  error:', 'color: #0e93e0;background: #aaefe5;', error);
-    }
+    launchImageLibrary(
+      { mediaType: 'photo', selectionLimit: 1 },
+      async data => {
+        if (data.assets?.length) {
+          try {
+            await WeChat.shareLocalImage({
+              imageUrl: data.assets[0].uri || '',
+              scene: 0,
+            });
+          } catch (error) {
+            console.log('error:', error);
+          }
+        }
+      },
+    );
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <View style={{ height: 200 }}>
-        <Button onPress={handleRegisterApp} title="registerApp" />
-        <Button onPress={handleOpenWXApp} title="openWXApp" />
-        <Button onPress={handleIsWXAppInstalled} title="isWXAppInstalled" />
-        <Button onPress={handleSendAuthRequest} title="授权登录" />
-        <Button onPress={handleAuthByScanRequest} title="扫码登录" />
-        {qrcode && (
-          <Image style={{ width: 100, height: 100 }} source={{ uri: qrcode }} />
-        )}
-        <Button onPress={handleShareText} title="shareText" />
-        <Button onPress={handleShareRemoteImage} title="shareRemoteImage" />
-        <Button onPress={handleShareLocalImage} title="shareLocalImage" />
-        <Button onPress={handlePay} title="pay" />
-        <View style={{ flex: 1, marginTop: 100 }}>
-      <View style={{
-        width: 160,
-        height: 36,
-        backgroundColor: 'hsl(190,50%,70%)',
-        paddingHorizontal: 16,
-        paddingVertical: 8,
-        borderRadius: 8
-      }} onTouchEnd={() => {
-        launchImageLibrary({ mediaType: 'photo', selectionLimit: 1 }, async (data) => {
-          if (data.assets?.length) {
-            console.log('%c WechatLibTurboModuleLogger data.assets:', 'color: #0e93e0;background: #aaefe5;', data.assets);
-            try {
-              const res = await WeChat.shareLocalImage({
-                imageUrl: data.assets[0].uri || '',
-                scene: 0,
-              });
-              console.log('%c WechatLibTurboModuleLogger handleShareRemoteImage res:', 'color: #0e93e0;background: #aaefe5;', res);
-            } catch (error) {
-              console.log('%c  error:', 'color: #0e93e0;background: #aaefe5;', error);
-            }
-          }
-        })
-      }}>
-        <Text style={{ width: '100%', height: '100%', fontWeight: 'bold', textAlign: 'center' }}>分享本地图片</Text>
-      </View>
-    </View>
-      </View>
-    </ScrollView>
+    <Tester style={{ flex: 1 }}>
+      <ScrollView>
+        <TestSuite name="注册应用">
+          <TestCase itShould="Click the button to register the app, and true will pop up when successful.">
+            <Button title="registerApp" onPress={handleRegisterApp} />
+          </TestCase>
+        </TestSuite>
+        <TestSuite name="打开微信">
+          <TestCase itShould="Click the button to launch the WeChat application">
+            <Button title="openWXApp" onPress={handleOpenWXApp} />
+          </TestCase>
+        </TestSuite>
+        <TestSuite name="授权登录">
+          <TestCase itShould="Click the button to bring up the WeChat authorization interface, and return the authorization information after authorization.">
+            <View>
+              <Button title="sendAuthRequest" onPress={handleSendAuthRequest} />
+              {authRes && <Text>授权登录结果: {JSON.stringify(authRes)}</Text>}
+            </View>
+          </TestCase>
+        </TestSuite>
+        <TestSuite name="扫码登录">
+          <TestCase itShould="Click the button to get the QR code to log in. Scan the QR code to authorize the login and return the login information.">
+            <View>
+              <Button title="authByScan" onPress={handleAuthByScanRequest} />
+              {qrcode && !scanRes && (
+                <Image
+                  style={{ width: 100, height: 100 }}
+                  source={{ uri: qrcode }}
+                />
+              )}
+              {scanRes && <Text>扫码登录结果: {JSON.stringify(scanRes)}</Text>}
+            </View>
+          </TestCase>
+        </TestSuite>
+        <TestSuite name="分享文本">
+          <TestCase
+            itShould="
+Click the button to pull up the WeChat chat list and select friends to share text">
+            <Button title="shareText" onPress={handleShareText} />
+          </TestCase>
+        </TestSuite>
+        <TestSuite name="分享图片">
+          <TestCase
+            itShould="
+Click the button to pull up the WeChat chat list and select friends to share remote image">
+            <Button title="shareImage" onPress={handleShareRemoteImage} />
+          </TestCase>
+        </TestSuite>
+        <TestSuite name="分享本地图片">
+          <TestCase
+            itShould="
+Click the button to select a local picture, then pull up the WeChat chat list and select friends to share the picture">
+            <Button title="shareLocalImage" onPress={handleShareLocalImage} />
+          </TestCase>
+        </TestSuite>
+        <View style={{ height: 150 }} />
+      </ScrollView>
+    </Tester>
   );
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    flex: 1,
-    paddingTop: 30,
-    backgroundColor: '#f0f0f0',
+  buttonContainer: {
+    width: 80,
+    height: 80,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  text: {
+    height: 20,
+    width: 200,
+    fontSize: 14,
   },
 });
