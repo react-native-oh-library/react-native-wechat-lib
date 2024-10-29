f4ca328c0475f0298b60d23284490b2df814dc21
zhengjinshou <>
2024-10-29 19:36:53 +0800

    feat: 添加 pay 用例

--- example/App.tsx
+++ example/App.tsx
@@ -9,7 +9,7 @@ import {
   Image,
 } from 'react-native';
 import { Tester, TestCase, TestSuite } from '@rnoh/testerino';
-import * as WeChat from '@react-native-ohos/react-native-wechat-lib';
+import * as WeChat from 'react-native-wechat-lib';
 import { APP_ID, APP_SECRET } from './src/constants';
 import { launchImageLibrary } from 'react-native-image-picker';
 
@@ -42,7 +42,7 @@ const App = () => {
 
   const handleAuthByScanRequest = async () => {
     try {
-      const res = await WeChat.authByScan(APP_ID, APP_SECRET, qrcode => {
+      const res = await WeChat.authByScan(APP_ID, APP_SECRET, (qrcode: any) => {
         // 拿到 qrcode 用 Image 去渲染
         setQrcode(qrcode);
       });
@@ -96,6 +96,23 @@ const App = () => {
         }
       },
     );
+  };
+
+  const handlePay = async () => {
+    try {
+      const res = await WeChat.pay({
+        partnerId: '2480306091',
+        prepayId: 'wx26161523845794ecced251acf2b6860000',
+        nonceStr: 'vmall_240926161523_993_2774',
+        timeStamp: '1727338524',
+        package: 'Sign=WXPay',
+        sign: 'rAqsrx5yLfRNBGvlHYuLhUsNK0OPeOLQ5xlvhxFo9guPU4HeNtzRdPaGAXAzXvn7V5chVe8sj3BfvDgwXlCKctCcFIllOgheyZbZ7btFC',
+        extData: '',
+      });
+      console.log(res);
+    } catch (error) {
+      console.log('%c  error:', 'color: #0e93e0;background: #aaefe5;', error);
+    }
   };
 
   return (
@@ -152,6 +169,11 @@ Click the button to pull up the WeChat chat list and select friends to share rem
             itShould="
 Click the button to select a local picture, then pull up the WeChat chat list and select friends to share the picture">
             <Button title="shareLocalImage" onPress={handleShareLocalImage} />
+          </TestCase>
+        </TestSuite>
+        <TestSuite name="支付">
+          <TestCase itShould="Click the button to launch WeChat payment">
+            <Button title="pay" onPress={handlePay} />
           </TestCase>
         </TestSuite>
         <View style={{ height: 150 }} />
